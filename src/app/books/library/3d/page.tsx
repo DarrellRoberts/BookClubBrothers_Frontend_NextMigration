30e57ea4c3e754ec15574f5b7e3026172d3502ab
DarrellRoberts <mrdarrellroberts@gmail.com>
2024-08-06 18:10:03 +0200

    00011: created route for 3d feature

---
+++ src/app/books/library/3d/page.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/react-in-jsx-scope */
+import style from "./3d.module.css";
+
+const Book3D: React.FC = () => {
+  return (
+    <h1 className={style.title}>Coming (very) Soon!</h1>
+  );
+};
+
+export default Book3D;
