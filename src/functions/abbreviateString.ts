8b52458f54fa052f8c500875775909fdd1eecb2d
DarrellRoberts <mrdarrellroberts@gmail.com>
2024-10-11 20:42:22 +0200

    00036: simplified abbreviation function

--- src/functions/abbreviateString.ts
+++ src/functions/abbreviateString.ts
@@ -1,8 +1,8 @@
 const abbreviateString = (title: string) => {
   let abbLabel: string = "";
   const arr: string[] = title.split(" ");
-  for (let i = 0; i < arr.length; i++) {
-    abbLabel = abbLabel + arr[i][0];
+  for (const word of arr) {
+    abbLabel = abbLabel + word[0];
   }
   return abbLabel;
 };
