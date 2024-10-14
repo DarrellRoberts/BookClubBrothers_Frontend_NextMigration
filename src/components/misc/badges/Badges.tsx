76e5a1cfdd49fc2796670f849e4e08d959ec6ed1
DarrellRoberts <mrdarrellroberts@gmail.com>
2024-10-14 12:34:58 +0200

    00041: added first book badge

--- src/components/misc/badges/Badges.tsx
+++ src/components/misc/badges/Badges.tsx
@@ -3,13 +3,15 @@ import BookWorm from "./BookWorm";
 import LoneWolf from "./LoneWolf";
 import style from "./Badges.module.css";
 import CommentWorm from "./CommentWorm";
+import FirstBook from "./FirstBook";
 
 type Props = {
   badgeData: {
     loneWolf: boolean,
     allBooks: boolean,
     mostBooks: boolean,
-    fiveComments: boolean
+    fiveComments: boolean,
+    firstBook: boolean
   };
 };
 
@@ -17,6 +19,7 @@ const Badges: React.FC<Props> = ({ badgeData}) => {
 
   return (
     <div className={style.badgesCon}>
+      <FirstBook firstBookWorm={badgeData?.firstBook} />
       <BookWorm  bookWorm={badgeData?.allBooks}/>
       <LoneWolf  loneWolf={badgeData?.loneWolf}/>
       <CommentWorm commentWorm={badgeData?.fiveComments} />
