7b7f32e2f25f2edd9a2acc2f857c40040acc5b39
DarrellRoberts <mrdarrellroberts@gmail.com>
2025-04-11 19:10:53 +0200

    chor: added form validation to create book randomiser

--- src/hooks/crud-hooks/useForm.ts
+++ src/hooks/crud-hooks/useForm.ts
@@ -1,19 +1,19 @@
-"use client";
+"use client"
 
-import { useAppSelector } from "@/store/lib/hooks";
-import { useState } from "react";
+import { useAppSelector } from "@/store/lib/hooks"
+import { useState } from "react"
 
 const useForm = (url: string, reqType: string, customData?: object) => {
-  const [loadings, setLoadings] = useState(false);
-  const [error, setError] = useState("");
+  const [loadings, setLoadings] = useState(false)
+  const [error, setError] = useState("")
 
-  const token = useAppSelector((state) => state.token.tokenState);
+  const token = useAppSelector((state) => state.token.tokenState)
   const formData =
-    customData ?? useAppSelector((state) => state.bookFormData.formData);
+    customData ?? useAppSelector((state) => state.bookFormData.formData)
 
   const handleSubmit = async () => {
     try {
-      setError(null);
+      setError(null)
       const response = await fetch(url, {
         method: reqType,
         headers: {
@@ -21,33 +21,33 @@ const useForm = (url: string, reqType: string, customData?: object) => {
           Authorization: `Bearer ${token}`,
         },
         body: reqType === "DELETE" ? null : JSON.stringify(formData),
-      });
-      const data = await response.json();
+      })
+      const data = await response.json()
       if (!response.ok) {
-        console.log(data);
-        console.log(token);
-        setError(data);
-        console.log("something has happened");
+        console.log(data)
+        console.log(token)
+        setError(data)
+        console.log("something has happened")
       }
 
       if (response.ok) {
-        console.log("success");
+        console.log("success")
       }
     } catch (error) {
-      setError(error);
-      console.log(error);
+      setError(error)
+      console.log(error)
     }
-  };
+  }
 
   const enterLoading = () => {
-    setLoadings(true);
+    setLoadings(true)
     setTimeout(() => {
-      setLoadings(false);
-      document.location.reload();
-    }, 4000);
-  };
+      setLoadings(false)
+      document.location.reload()
+    }, 4000)
+  }
 
-  return { handleSubmit, error, enterLoading, loadings };
-};
+  return { handleSubmit, error, enterLoading, loadings, setError }
+}
 
-export default useForm;
+export default useForm
