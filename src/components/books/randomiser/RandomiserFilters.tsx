ff98c48e16d8f9706694e858aeee59527f194798
DarrellRoberts <mrdarrellroberts@gmail.com>
2025-06-19 17:25:59 +0200

    fix: fixed filters styling & logic

--- src/components/books/randomiser/RandomiserFilters.tsx
+++ src/components/books/randomiser/RandomiserFilters.tsx
@@ -2,6 +2,8 @@ import { Book } from "@/types/BookInterface"
 import { User } from "@/types/UserInterface"
 import React, { useEffect, useState } from "react"
 import "./filters.css"
+import { useAppDispatch } from "@/store/lib/hooks"
+import { setIndex } from "@/store/lib/features/randomise/randomiseSlice"
 
 type Props = {
   bookData: Book[]
@@ -16,6 +18,8 @@ const RandomiserFilters: React.FC<Props> = ({
 }) => {
   const [nameFilter, setNameFilter] = useState([])
   const [genreFilter, setGenreFilter] = useState([])
+
+  const dispatch = useAppDispatch()
 
   const genreList = [
     "Horror",
@@ -57,43 +61,66 @@ const RandomiserFilters: React.FC<Props> = ({
   useEffect(() => {
     if (nameFilter.length === 0 && genreFilter.length === 0) {
       setBookData(bookData)
+      dispatch(setIndex(0))
     } else if (nameFilter.length === 0 && genreFilter.length > 0) {
       const tempArray = bookData?.filter((book) =>
         book.genre[0].some((genre) => genre.includes(genreFilter))
       )
       setBookData(tempArray)
+      dispatch(setIndex(0))
     } else if (nameFilter.length > 0 && genreFilter.length === 0) {
       const tempArray = bookData?.filter((book) =>
         nameFilter?.includes(book.suggestedBy)
       )
       setBookData(tempArray)
+      dispatch(setIndex(0))
     } else if (nameFilter.length > 0 && genreFilter.length > 0) {
       let tempArray = bookData?.filter((book) =>
         nameFilter?.includes(book.suggestedBy)
       )
       tempArray = tempArray?.filter((book) =>
-        book.genre[0].some((genre) => genre.includes(genreFilter))
+        genreFilter.every((selectedGenre) =>
+          book.genre[0].includes(selectedGenre)
+        )
       )
       setBookData(tempArray)
+      console.log(tempArray)
+      dispatch(setIndex(0))
     }
   }, [nameFilter, genreFilter])
+  console.log(genreFilter)
   return (
     <div className="flex flex-col">
       <div>
         <h2 className="text-center">Filter by suggestor</h2>
-        <div className="flex w-full justify-evenly">
+        <div className="name-filter-grid">
           {userData?.map((user) => (
             <div key={user._id}>
               <input
-              className="filter-checkbox"
+                className="filter-checkbox"
                 type="checkbox"
+                checked={nameFilter.includes(user._id)}
                 name={user.username}
                 value={user._id}
                 onChange={() => handleNameCheckbox(user._id)}
               />
-              <label htmlFor={user.username} className="filter-label">{user.username}</label>
+              <label htmlFor={user.username} className="filter-label">
+                {user.username}
+              </label>
             </div>
           ))}
+          <div>
+            <input
+              type="checkbox"
+              className="filter-checkbox"
+              name="Clear all"
+              checked={nameFilter.length === 0}
+              onChange={() => setNameFilter([])}
+            />
+            <label htmlFor="Clear all" className="filter-label">
+              Clear all
+            </label>
+          </div>
         </div>
       </div>
       <div className="mt-[3rem] mb-[3rem]">
@@ -104,13 +131,28 @@ const RandomiserFilters: React.FC<Props> = ({
               <input
                 type="checkbox"
                 className="filter-checkbox"
+                checked={genreFilter.includes(genre)}
                 name={genre}
                 value={genre}
                 onChange={() => handleGenreCheckbox(genre)}
               />
-              <label htmlFor={genre} className="filter-label">{genre}</label>
+              <label htmlFor={genre} className="filter-label">
+                {genre}
+              </label>
             </div>
           ))}
+          <div>
+            <input
+              type="checkbox"
+              className="filter-checkbox"
+              name="Clear all"
+              checked={genreFilter.length === 0}
+              onChange={() => setGenreFilter([])}
+            />
+            <label htmlFor="Clear all" className="filter-label">
+              Clear all
+            </label>
+          </div>
         </div>
       </div>
     </div>
